84a5cae3cc64415c430a574cf13e5c663651be54
Andrew Hunton <huntonas@gmail.com>
2014-09-12 14:01:27 -0500

    Add activate function. Add row to wp_options table

--- easy-subscribe.php
+++ easy-subscribe.php
@@ -11,7 +11,7 @@
 /*  Copyright 2014  Andrew Hunton  (email : huntonas@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -23,9 +23,35 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+?>
 
+<?php
 
 defined('ABSPATH') or die("No script kiddies please!");
+
+
+register_activation_hook(__FILE__, 'easySubscribeInstall');
+
+function easySubscribeInstall() {
+
+  global $wpdb;
+
+  $data = array (
+    'option_name' => 'easy_subscribe_emails'
+  );
+
+
+  $success = $wpdb->insert('wp_options', $data);
+
+  if($success) {
+    echo 'Row successfully creates';
+  }
+  else {
+    wp_die('Something went Terribly wrong here');
+  }
+
+}
+
 
 plugin_dir_path(__FILE__); //Local path to the plugin
 
