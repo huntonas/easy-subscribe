505fed9cc29df74f2ec33c81d0de9e46eb3dda31
Andrew Hunton <ahunton@gbhem.org>
2014-09-12 13:09:09 -0500

    Edit readme.txt

--- easy-subscribe.php
+++ easy-subscribe.php
@@ -8,5 +8,25 @@
  * License: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
+/*  Copyright 2014  Andrew Hunton  (email : huntonas@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 defined('ABSPATH') or die("No script kiddies please!");
+
+plugin_dir_path(__FILE__); //Local path to the plugin
+
+sanitize_email($email); //Sanitizing the email address
